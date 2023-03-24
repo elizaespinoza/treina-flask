a8e8b8eb3039f389fc8305e39fe725e6a47d705e
Elizabeth Alfaro-Espinoza <er.alfaroespinoza@gmail.com>
2023-03-24 10:06:38 -0300

    fix: remove app pycache and ignore it in future commits
    master

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,25 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Testando Flask!"
+    user = {"username": "Eliza"}
+    posts = [
+        {
+            "author": {"username": "Lucas"},
+            "body": "Beautiful day in Curitiba!",
+        },
+        {
+            "author": {"username": "Ana"},
+            "body": "The Ghibli studio's movie was so cool!",
+        },
+    ]
+    return render_template(
+        "index.html",
+        title="Home",
+        user=user,
+        posts=posts,
+    )
